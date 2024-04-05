8efa58a5918677bf761e9fd5d5d3e426488f3fa6
apsranasuriya <apsranasuriya@myseneca.ca>
2024-04-05 13:43:03 -0400

    new changes

--- Question2.js
+++ Question2.js
@@ -6,6 +6,12 @@ const port = 3000;
 
 
 app.use(express.json());
+const handlebars = require('express-handlebars');
+app.engine('hbs', handlebars.engine({ extname: '.hbs', }));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,6 +36,27 @@ app.get('/alldata', (req, res) => {
         }
     });
 
+});
+
+app.get('/viewdata', (req, res) => {
+    fs.readFile('country.json', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        try {
+            const jsonData = JSON.parse(data);
+            res.render('viewdata', {
+                countries: jsonData.country,
+                layout: false
+            });
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+            res.status(500).send('Internal Server Error');
+        }
+    });
 });
 
 app.listen(port, () => {
