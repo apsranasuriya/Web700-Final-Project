dd2802dfc1290bc181deb6b627ab7c420967ca60
apsranasuriya <apsranasuriya@myseneca.ca>
2024-04-16 15:52:36 -0400

    git

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const app = express();
 // const port = 3000|process.env.PORT;
-var HTTP_PORT = 3000|process.env.PORT;
+var HTTP_PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
